c0889780ce28b07ef59839a19f74a91ef7fd6b82
Shravanis1234* <shravanisecerymec@gmail.com>
2021-02-19 16:50:23 +0530

    Update

---
+++ String.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+class string
+{
+public static void main(String[] args)
+{
+String s=new string();//without new operator
+String s1="kavita";
+String s2="hema";
+String s3="kavita";
+String s4=new String("kavita")://with new operator
+String s5=new String("latha"):
+System.out.println(s3.length());
+}
+}
---
+++ stringex.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+class stringex
+{
+public static void main(String[] args)
+{
+String s=new string();//without new operator
+String s1="kavita";
+String s2="hema";
+String s3="kavita";
+System.out.println("s3.length());
+}
+}
